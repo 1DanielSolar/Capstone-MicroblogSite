df4bef1462282a06aa604ef11135ab62516e6680
1DanielSolar <danielsolar2003@gmail.com>
2024-06-26 10:16:59 -0700

    Initial commit

---
+++ scripts/registration.js
@@ -0,0 +1,68 @@
+/* Landing Page JavaScript */
+
+"use strict";
+
+// After the window loads and after the register button is clicked...
+window.addEventListener('load', function () {
+  this.document.getElementById("register").onclick = () => {
+    // Values of Input Fields
+    const username = document.getElementById("username").value;
+    const pw = document.getElementById("psw").value;
+    const confpw = document.getElementById("psw-repeat").value;
+    const fullNameVal = this.document.getElementById("fullName").value;
+    // TODO: CALL ON CLEAR MSG FUNCTION
+    clearMsg();
+
+    const isEmpty = (!(username && pw && confpw))
+    if (!isEmpty && (pw === confpw)) {
+      console.log("All fields were successfully filled.")
+      const myHeaders = new Headers();
+      myHeaders.append("Content-Type", "application/json");
+
+      const raw = JSON.stringify({
+        "username": username,
+        "fullName": fullNameVal,
+        "password": pw
+      });
+
+      const options = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+      };
+      fetch(api + "/api/users", options)
+      .then(response => {
+          if(!response.ok) {
+              throw new Error(`HTTP error! status: ${response.status}`);
+          }
+          return response.json()
+      })
+      .then(json => {
+          setTimeout(
+              function () {
+                  window.location.assign("index.html");
+              }, 5000);
+         msg.innerHTML = "User created successfully, Redirecting to Log In page"
+  
+      })
+      .catch(error => {
+        msg.innerHTML = "Something went wrong... check console."
+          console.error("Error:", error);
+      });
+  
+    } 
+    else {
+      msg.innerHTML = "Something went wrong... check console."
+      if (isEmpty) {
+        console.log("Check for blank fields")
+      } else if (pw !== confpw) {
+        console.log("Passwords don't match.")
+      }
+    }
+  }
+})
+
+function clearMsg() {
+  const msg = document.getElementById("msg")
+  msg.innerHTML = ""
+}
